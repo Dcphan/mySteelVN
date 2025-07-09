357f3da91eb3cfbe127e226327d54a426485eee4
Dcphan <phanchauduc1711@gmail.com>
2025-07-09 14:25:23 +0700

    change

---
+++ backend/ETL_pipeline/import_pipeline.py
@@ -0,0 +1,43 @@
+from backend.ETL_pipeline import parser, regex_pattern, country_detection, country_dictionary, hs_and_currency, hs_code, unit, unit_conversion
+import pandas as pd
+
+
+class imported_pipeline():
+    def __init__(self, import_file: object , address: list,  country_detect: object, hs_and_currency_converter: object, convert_unit: object):
+        self.address_cols = address
+        self.import_file = import_file
+        self.country_detect = country_detect
+        self.hs_and_currency_converter = hs_and_currency_converter
+        self.convert_unit = convert_unit
+    
+    def produce_check_xlsx(self):
+        excel = self.import_file[['DATE', 'TAX CODE', 'IMPORTER', 'ADDRESS', 'EXPORTER', 'HS CODE', 'CURRENCY', 'AMOUNT', 'UNIT PRICE', 'UNIT']] # Name in Excel Have To Match
+
+        excel["EXCHANGE RATE"] = self.import_file["EXCHANGE RATE"].apply(lambda x: self.hs_and_currency_converter.ti_gia_conversion(x))
+
+        excel['UNIT PRICE'] = excel.apply(lambda x: self.hs_and_currency_converter.unit_conversion(unit_price = x['UNIT PRICE'], forex_rate = x['EXCHANGE RATE'], unit = x['UNIT']), axis=1)
+
+        excel['COMBINED ADRESS'] = self.import_file.apply(
+            lambda row: self.country_detect.combine_address(
+                row['DIA CHI 1'],
+                row['DIA CHI 2'],
+                row['DIA CHI 3'],
+                row['DIA CHI 4']
+            ),
+            axis=1
+        )
+
+        excel["COMMODITY"] = excel["HS CODE"].apply(lambda x: hs_code.hs_code_dict.get(x, None))
+
+        excel['EXPORTED COUNTRY'] = self.import_file.apply(
+            lambda row: self.country_detect.detect_country_full([row[col] for col in self.address_cols]),
+            axis=1
+        )
+
+        excel['QUANTITY'] = self.import_file.apply(lambda row: self.convert_unit.convert_to_tan(row["QUANTITY"], row["UNIT"]), axis=1)
+
+        importer_i_df = excel[['TAX CODE', 'IMPORTER', 'ADDRESS']]
+        
+        
+
+        excel.to_excel("output.xlsx", index=False)
