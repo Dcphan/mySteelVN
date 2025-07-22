d0f29fd26dbaca842fa5404a2eb919267b32f11d
Dcphan <phanchauduc1711@gmail.com>
2025-07-22 17:04:36 +0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -4,13 +4,14 @@ from fastapi.responses import HTMLResponse, JSONResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Optional
 from table_db import TableDatabase
 from db import SteelDatabaseManager
 from Import import SteelDataProcessor
 from pydantic import BaseModel
 from pathlib import Path
 import uvicorn
+from xnk_pipeline import XNK_pipeline
 from starlette.background import BackgroundTask
 from export import Export
 import shutil
@@ -173,6 +174,86 @@ def get_product_data():
     print("Returned product data:", data)
     return data
 
+
+# FILE XUAT NHAP KHAU
+export_handler = Export(dbname="steel_database", user="mysteelvn", password="cjLVuBdaSd5vtst")
+
+@app.get('/upload_selection')
+def upload_selection_page(request: Request):
+    return templates.TemplateResponse("xnk_upload.html", {"request": request})
+
+@app.get('/upload_single_address')
+def upload_single_address_page(request: Request):
+    return templates.TemplateResponse("upload_single_address.html", {"request": request})
+
+@app.get('/upload_multi_address')
+def upload_multi_address_page(request: Request):
+    return templates.TemplateResponse("upload_multi_address.html", {"request": request})
+
+@app.post("/upload_excel_xnk")
+async def upload_excel_file(file: UploadFile = File(...)):
+    try:
+        temp_dir = os.path.join(BASE_DIR, "temp_uploads")
+        os.makedirs(temp_dir, exist_ok=True)
+        temp_path = os.path.join(temp_dir, file.filename)
+
+        with open(temp_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        processor = XNK_pipeline(file_path=temp_path)
+        processor.import_function()
+
+        return {"message": f"✅ File '{file.filename}' uploaded and processed."}
+
+    except Exception as e:
+        return {"error": f"❌ Failed to process file: {str(e)}"}
+
+    finally:
+        if os.path.exists(temp_path):
+            os.remove(temp_path)
+
+@app.get("/api/pivot-data")
+def pivot_summary(
+      filter_field: Optional[str] = Query(
+        default=None,
+        examples={"example": {"value": "commodity"}}
+    ),
+    filter_value: Optional[str] = Query(
+        default=None,
+        examples={"example": {"value": "Alloy Bar"}}
+    ),
+    rows_field: List[str] = Query(default=["country", "company"]),
+    rows_value: List[str] = Query(default=["Japan", "China"]),
+    value_fields: List[str] = Query(default=["quantity", "amount"]),
+    date: str = Query(default="2025-05")
+):
+    data = db_manager.xnk_get_info(
+        filter_field=filter_field,
+        filter_value=filter_value,
+        rows_fields=rows_field,
+        rows_values=rows_value,
+        values_fields=value_fields,
+        date=date
+    )
+    return JSONResponse(content=data)
+
+@app.get("/api/single-pivot-summary")
+def pivot_country_summary(row_field: str=Query(...), date: str=Query(...), item: Optional[List]=Query(None), value_fields: List[str]=Query(...)):
+    data = db_manager.xnk_get_total_data(row_field, date, item, value_fields)
+    return data
+
+@app.get("/table/pivot-table")
+def read_table(request: Request):
+    return templates.TemplateResponse("pivottable.html", {"request": request})
+
+
+@app.get("/api/filtering-data")
+def filtering_data(
+    filter: str = Query(default=None),
+    date: str = Query(default=None),
+):
+    data = db_manager.xnk_get_distinct_filter(filter, date)
+    return data
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
