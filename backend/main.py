77be839757b3f996dde7cc2a8094e2a78e2ecd4f
Dcphan <phanchauduc1711@gmail.com>
2025-07-28 10:47:35 +0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -254,6 +254,3 @@ def filtering_data(
 ):
     data = db_manager.xnk_get_distinct_filter(filter, date)
     return data
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
